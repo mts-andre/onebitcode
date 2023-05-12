e98e1efcd7fd43ada0811d5cb816a7de691f92d3
Matheus André <122615835+mts-andre@users.noreply.github.com>
2023-05-12 15:47:01 -0300

    Add files via upload

---
+++ javascript/projects/jobs/script.js
@@ -0,0 +1,4 @@
+function exibirMenu() {
+    const func = prompt('Escolha uma das opções: \n1 - Listar vagas disponíveis \n2 - Criar uma nova vaga \n3 - Visualizar uma vaga \n4 - Inscrever um candidato em uma vaga \n5 - Excluir vaga \n6 - Sair')
+}
+
