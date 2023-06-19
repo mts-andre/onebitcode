5e631dd6ce83dd7af0e35e89cb497fb8f62242cc
Matheus André <122615835+mts-andre@users.noreply.github.com>
2023-06-19 15:40:08 -0300

    Add files via upload

---
+++ js I/ex1/script.js
@@ -0,0 +1,6 @@
+let nome = prompt('Digite seu nome:')
+let ultimoNome = prompt('Digite seu sobrenome:')
+let campoDeEstudo = prompt('Digite seu campo de estudo:')
+let anoNascimento = parseInt(prompt('Digite seu ano de nascimento:'))
+
+console.log(`Nome: ${nome} ${ultimoNome} \nCampo de estudo: ${campoDeEstudo} \nIdade: ${2023 - anoNascimento}`)
---
+++ js I/ex2/script.js
@@ -0,0 +1,4 @@
+let numeroA = parseInt(prompt('Digite o valor de A'))
+let numeroB = parseInt(prompt('Digite o valor de B'))
+
+console.log(`Soma: ${numeroA + numeroB} \nSubtração: ${numeroA - numeroB} \nMultiplicação: ${numeroA * numeroB} \nDivisão: ${numeroA / numeroB}`)
