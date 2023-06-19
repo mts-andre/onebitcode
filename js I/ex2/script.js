ea8fd5e994218eb72c53454b6632265857fdac3b
Matheus André <122615835+mts-andre@users.noreply.github.com>
2023-06-19 15:41:35 -0300

    Update script.js

--- js I/ex2/script.js
+++ js I/ex2/script.js
@@ -1,3 +1,9 @@
+/*
+  Escreve um programa em javascript que permita inserir dois valores numéricos e então calcule o resultado das quatro operações básicas (soma, subtração, multiplicação e divisão).  
+  
+  Após calcular os resultados o programa deve exibi-los na tela.
+*/
+
 let numeroA = parseInt(prompt('Digite o valor de A'))
 let numeroB = parseInt(prompt('Digite o valor de B'))
 
