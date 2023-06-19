9b6e71f4a540946248e78b2f6c5a6ce757eda2b6
Matheus André <122615835+mts-andre@users.noreply.github.com>
2023-06-19 15:41:00 -0300

    Update script.js

--- js I/ex1/script.js
+++ js I/ex1/script.js
@@ -1,3 +1,14 @@
+/*
+  Escreva um programa em javascript que permita salvar informações de um recruta. As informações a serem salvas são:
+
+  - o primeiro nome
+  - o sobrenome
+  - o campo de estudo
+  - o ano de nascimento
+  
+  Depois o programa deve exibir o nome completo do recruta, seu campo de estudo e sua idade (apenas baseada no ano de nascimento).
+*/
+
 let nome = prompt('Digite seu nome:')
 let ultimoNome = prompt('Digite seu sobrenome:')
 let campoDeEstudo = prompt('Digite seu campo de estudo:')
